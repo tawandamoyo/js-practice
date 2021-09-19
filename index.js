4fc7dc5057ed2f2824d1899b8ff290c2ccd11c45
Tawanda Moyo <moyotawanda@gmail.com>
2021-09-19 18:09:47 +0200

    Add solutions

---
+++ index.js
@@ -0,0 +1,30 @@
+const exercises = [
+    {
+      id: 1,
+      title: "Bench Press (Barbell)",
+      muscle_group: "chest",
+      other_muscles: ["triceps", "shoulders"],
+      type: "weight_reps",
+    },
+    {
+      id: 2,
+      title: "Military Press",
+      muscle_group: "shoulders",
+      other_muscles: ["triceps"],
+      type: "weight_reps",
+    },
+    {
+      id: 3,
+      title: "Incline Bench Press (Barbell)",
+      muscle_group: "chest",
+      other_muscles: ["shoulders"],
+      type: "weight_reps",
+    },
+    {
+      id: 4,
+      title: "Push Up",
+      muscle_group: "chest",
+      other_muscles: ["triceps", "shoulders"],
+      type: "reps_only",
+    },
+  ]
