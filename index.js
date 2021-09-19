242a3de6b48ca8ece05c87de504006892d161825
Tawanda Moyo <moyotawanda@gmail.com>
2021-09-19 19:44:55 +0200

    Update solutions

--- index.js
+++ index.js
@@ -28,3 +28,57 @@ const exercises = [
       type: "reps_only",
     },
   ]
+
+
+  /**
+ * Problem 1:
+ * 
+ * Using the exercises array, create an array that contains
+ * the titles of all the exercises of type "weight_reps".
+ * 
+ * Use console.log to log the result.
+ */
+
+//   Solution:
+
+let weightRepsExerciseTitles = exercises
+  .map((exercise) => {
+      if (exercise.type === "weight_reps") {
+          return exercise.title
+      } 
+  })
+  .filter(Boolean); // Removes undefined values, since Map() returns undefined for falsy values
+
+
+console.log('Solution 1: ')
+console.log(weightRepsExerciseTitles);
+
+/**
+ * Problem 2:
+ * 
+ * Imagine the user was filtering a list of exercises based on
+ * the target muscle group. Write a function that takes a muscle
+ * group ("triceps", "shoulders", "chest") and returns an array
+ * of all the exercises that use that muscles group.
+ * 
+ * A muslce_group is used if it's included in either the muscle_group
+ * or other_muscles props.
+ * 
+ * Use console.log to log the result.
+ */
+
+// Solution:
+
+
+ 
+function getMuscleGroupExercises(muscleGroup) {
+    let muscleGroupExercises = exercises.filter((exercise) => {
+        if (exercise.muscle_group === muscleGroup || exercise.other_muscles.includes(muscleGroup)) {
+            return exercise;
+        }
+    })
+        
+    return muscleGroupExercises;
+}
+console.log('\n Solution 2:')
+console.log(getMuscleGroupExercises('chest'));
